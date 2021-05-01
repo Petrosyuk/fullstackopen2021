179120fee94e1e605ad2d8d34baa31af44bca706
Anton Petrosyuk <antonpetrosyuk@Antons-Air.lan>
2021-04-30 20:26:29 -0400

    Extracted axios logic to services folder

---
+++ part2/notes/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteServices from "./services/notes";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    noteServices.getAll().then((rsp) => setNotes(rsp.data));
+  }, []);
+
+  const toggleImportanceOf = (id) => {
+    console.log(notes, `importantce of ${id} needs to be toggled`);
+
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteServices.update(id, changedNote).then((rsp) => {
+      console.log(`${id} has been toggled`, rsp.data);
+      setNotes(notes.map((note) => (note.id !== id ? note : rsp.data)));
+    });
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
